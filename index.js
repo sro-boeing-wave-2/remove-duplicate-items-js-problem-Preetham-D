851ea74dc0f8914077aaf31e00bc36cc3f462006
Preetham D <preethamdbangera@gmail.com>
2018-07-09 16:36:26 +0530

    remove-duplicate

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
+const removeDuplicateItems = (items) => {
+  const set1 = new Set(items);
+  const array = Array.from(set1);
+  return (array);
+};
 
 module.exports = removeDuplicateItems;
